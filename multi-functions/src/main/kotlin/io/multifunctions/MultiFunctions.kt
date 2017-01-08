6baab02dabbf9faf037533e54b0e5df822779a89
Benny Schneider <stupacki86@gmail.com>
2017-01-08 14:03:40 +0100

    adding MultiMapNotNul

--- multi-functions/src/main/kotlin/io/multifunctions/MultiFunctions.kt
+++ multi-functions/src/main/kotlin/io/multifunctions/MultiFunctions.kt
@@ -3,6 +3,7 @@ package io.multifunctions
 interface MultiFunctions : MultiLet,
                            MultiLetNotNull,
                            MultiMap,
+                           MultiMapNotNull,
                            MultiFlatMap,
                            MultiMapIndexed,
                            MultiMapIndexedNotNull,
