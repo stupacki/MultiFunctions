7cf4c13114711f9610657ceb857caf4410bc6765
Christian Maier <cmaierrr@gmail.com>
2017-05-23 14:33:25 +0200

    Nullsafe letNotNull functions and remove deprecated interfaces

--- multi-functions/src/main/kotlin/io/multifunctions/MultiMapNotNull.kt
+++ multi-functions/src/main/kotlin/io/multifunctions/MultiMapNotNull.kt
@@ -2,44 +2,42 @@ package io.multifunctions
 
 import io.multifunctions.models.*
 
-interface MultiMapNotNull {
-    fun <A, B, R : Any> Iterable<Pair<A?, B?>>.mapNotNull(transform: (A?, B?) -> R): List<R> =
-            this.mapNotNull { elem ->
-                when (null) {
-                    elem.first, elem.second -> null
-                    else -> transform(elem.first, elem.second)
-                }
+fun <A, B, R : Any> Iterable<Pair<A?, B?>>.mapNotNull(transform: (A?, B?) -> R): List<R> =
+        this.mapNotNull { elem ->
+            when (null) {
+                elem.first, elem.second -> null
+                else -> transform(elem.first, elem.second)
             }
+        }
 
-    fun <A, B, C, R : Any> Iterable<Triple<A?, B?, C?>>.mapNotNull(transform: (A?, B?, C?) -> R): List<R> =
-            this.mapNotNull { elem ->
-                when (null) {
-                    elem.first, elem.second, elem.third -> null
-                    else -> transform(elem.first, elem.second, elem.third)
-                }
+fun <A, B, C, R : Any> Iterable<Triple<A?, B?, C?>>.mapNotNull(transform: (A?, B?, C?) -> R): List<R> =
+        this.mapNotNull { elem ->
+            when (null) {
+                elem.first, elem.second, elem.third -> null
+                else -> transform(elem.first, elem.second, elem.third)
             }
+        }
 
-    fun <A, B, C, D, R : Any> Iterable<Quad<A?, B?, C?, D?>>.mapNotNull(transform: (A?, B?, C?, D?) -> R): List<R> =
-            this.mapNotNull { elem ->
-                when (null) {
-                    elem.first, elem.second, elem.third, elem.fourth -> null
-                    else -> transform(elem.first, elem.second, elem.third, elem.fourth)
-                }
+fun <A, B, C, D, R : Any> Iterable<Quad<A?, B?, C?, D?>>.mapNotNull(transform: (A?, B?, C?, D?) -> R): List<R> =
+        this.mapNotNull { elem ->
+            when (null) {
+                elem.first, elem.second, elem.third, elem.fourth -> null
+                else -> transform(elem.first, elem.second, elem.third, elem.fourth)
             }
+        }
 
-    fun <A, B, C, D, E, R : Any> Iterable<Penta<A?, B?, C?, D?, E?>>.mapNotNull(transform: (A?, B?, C?, D?, E?) -> R): List<R> =
-            this.mapNotNull { elem ->
-                when (null) {
-                    elem.first, elem.second, elem.third, elem.fourth, elem.fifth -> null
-                    else -> transform(elem.first, elem.second, elem.third, elem.fourth, elem.fifth)
-                }
+fun <A, B, C, D, E, R : Any> Iterable<Penta<A?, B?, C?, D?, E?>>.mapNotNull(transform: (A?, B?, C?, D?, E?) -> R): List<R> =
+        this.mapNotNull { elem ->
+            when (null) {
+                elem.first, elem.second, elem.third, elem.fourth, elem.fifth -> null
+                else -> transform(elem.first, elem.second, elem.third, elem.fourth, elem.fifth)
             }
+        }
 
-    fun <A, B, C, D, E, F, R : Any> Iterable<Hexa<A?, B?, C?, D?, E?, F?>>.mapNotNull(transform: (A?, B?, C?, D?, E?, F?) -> R): List<R> =
-            this.mapNotNull { elem ->
-                when (null) {
-                    elem.first, elem.second, elem.third, elem.fourth, elem.fifth, elem.sixth -> null
-                    else -> transform(elem.first, elem.second, elem.third, elem.fourth, elem.fifth, elem.sixth)
-                }
+fun <A, B, C, D, E, F, R : Any> Iterable<Hexa<A?, B?, C?, D?, E?, F?>>.mapNotNull(transform: (A?, B?, C?, D?, E?, F?) -> R): List<R> =
+        this.mapNotNull { elem ->
+            when (null) {
+                elem.first, elem.second, elem.third, elem.fourth, elem.fifth, elem.sixth -> null
+                else -> transform(elem.first, elem.second, elem.third, elem.fourth, elem.fifth, elem.sixth)
             }
-}
+        }
--- multi-functions/src/main/kotlin/io/multifunctions/MultiWithIndex.kt
+++ multi-functions/src/main/kotlin/io/multifunctions/MultiWithIndex.kt
@@ -2,22 +2,17 @@ package io.multifunctions
 
 import io.multifunctions.models.*
 
-interface MultiWithIndex {
+fun <A, B, R> Iterable<Pair<A?, B?>>.withIndex(transform: (A?, B?) -> R): Iterable<IndexedValue<R?>> =
+        this.mapIndexed { index, elem -> IndexedValue(index, transform(elem.first, elem.second)) }
 
+fun <A, B, C, R> Iterable<Triple<A?, B?, C?>>.withIndex(transform: (A?, B?, C?) -> R): Iterable<IndexedValue<R?>> =
+        this.mapIndexed { index, elem -> IndexedValue(index, transform(elem.first, elem.second, elem.third)) }
 
-    fun <A, B, R> Iterable<Pair<A?, B?>>.withIndex(transform: (A?, B?) -> R): Iterable<IndexedValue<R?>> =
-            this.mapIndexed { index, elem -> IndexedValue(index, transform(elem.first, elem.second)) }
+fun <A, B, C, D, R> Iterable<Quad<A?, B?, C?, D?>>.withIndex(transform: (A?, B?, C?, D?) -> R): Iterable<IndexedValue<R?>> =
+        this.mapIndexed { index, elem -> IndexedValue(index, transform(elem.first, elem.second, elem.third, elem.fourth)) }
 
-    fun <A, B, C, R> Iterable<Triple<A?, B?, C?>>.withIndex(transform: (A?, B?, C?) -> R): Iterable<IndexedValue<R?>> =
-            this.mapIndexed { index, elem -> IndexedValue(index, transform(elem.first, elem.second, elem.third)) }
+fun <A, B, C, D, E, R> Iterable<Penta<A?, B?, C?, D?, E?>>.withIndex(transform: (A?, B?, C?, D?, E?) -> R): Iterable<IndexedValue<R?>> =
+        this.mapIndexed { index, elem -> IndexedValue(index, transform(elem.first, elem.second, elem.third, elem.fourth, elem.fifth)) }
 
-    fun <A, B, C, D, R> Iterable<Quad<A?, B?, C?, D?>>.withIndex(transform: (A?, B?, C?, D?) -> R): Iterable<IndexedValue<R?>> =
-            this.mapIndexed { index, elem -> IndexedValue(index, transform(elem.first, elem.second, elem.third, elem.fourth)) }
-
-    fun <A, B, C, D, E, R> Iterable<Penta<A?, B?, C?, D?, E?>>.withIndex(transform: (A?, B?, C?, D?, E?) -> R): Iterable<IndexedValue<R?>> =
-            this.mapIndexed { index, elem -> IndexedValue(index, transform(elem.first, elem.second, elem.third, elem.fourth, elem.fifth)) }
-
-    fun <A, B, C, D, E, F, R> Iterable<Hexa<A?, B?, C?, D?, E?, F?>>.withIndex(transform: (A?, B?, C?, D?, E?, F?) -> R): Iterable<IndexedValue<R?>> =
-            this.mapIndexed { index, elem -> IndexedValue(index, transform(elem.first, elem.second, elem.third, elem.fourth, elem.fifth, elem.sixth)) }
-
-}
+fun <A, B, C, D, E, F, R> Iterable<Hexa<A?, B?, C?, D?, E?, F?>>.withIndex(transform: (A?, B?, C?, D?, E?, F?) -> R): Iterable<IndexedValue<R?>> =
+        this.mapIndexed { index, elem -> IndexedValue(index, transform(elem.first, elem.second, elem.third, elem.fourth, elem.fifth, elem.sixth)) }
