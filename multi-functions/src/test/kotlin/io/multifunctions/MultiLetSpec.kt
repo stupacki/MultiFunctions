e641a7f0a67ae66fa6f71d77b7adaac86282425f
Benny Schneider <benny.schneider@rewe-digital.com>
2018-09-28 13:52:50 +0200

    Simplify MultiLet tests

--- multi-functions/src/test/kotlin/io/multifunctions/MultiLetSpec.kt
+++ multi-functions/src/test/kotlin/io/multifunctions/MultiLetSpec.kt
@@ -11,41 +11,45 @@ class MultiLetSpec : WordSpec() {
         "MultiLet" should {
 
             "produce a correct binding from Pair" {
+                val testData = Pair("one", "two")
 
-                Pair("one", "two").let { one, two ->
+                testData.let { one, two ->
                     one shouldBe "one"
                     two shouldBe "two"
 
                     Pair(one, two)
-                } shouldBe Pair("one", "two")
+                } shouldBe testData
             }
 
             "produce a correct binding from Triple" {
+                val testData = Triple("one", "two", "three")
 
-                Triple("one", "two", "three").let { one, two, three ->
+                testData.let { one, two, three ->
                     one shouldBe "one"
                     two shouldBe "two"
                     three shouldBe "three"
 
                     Triple(one, two, three)
-                } shouldBe Triple("one", "two", "three")
+                } shouldBe testData
             }
 
             "produce a correct binding from Quad" {
+                val testData = Quad("one", "two", "three", "four")
 
-                Quad("one", "two", "three", "four").let { one, two, three, four ->
+                testData.let { one, two, three, four ->
                     one shouldBe "one"
                     two shouldBe "two"
                     three shouldBe "three"
                     four shouldBe "four"
 
                     Quad(one, two, three, four)
-                } shouldBe Quad("one", "two", "three", "four")
+                } shouldBe testData
             }
 
             "produce a correct binding from Penta" {
+                val testData = Penta("one", "two", "three", "four", "five")
 
-                Penta("one", "two", "three", "four", "five").let { one, two, three, four, five ->
+                testData.let { one, two, three, four, five ->
                     one shouldBe "one"
                     two shouldBe "two"
                     three shouldBe "three"
@@ -53,12 +57,13 @@ class MultiLetSpec : WordSpec() {
                     five shouldBe "five"
 
                     Penta(one, two, three, four, five)
-                } shouldBe Penta("one", "two", "three", "four", "five")
+                } shouldBe testData
             }
 
             "produce a correct binding from Hexa" {
+                val testData = Hexa("one", "two", "three", "four", "five", "six")
 
-                Hexa("one", "two", "three", "four", "five", "six").let { one, two, three, four, five, six ->
+                testData.let { one, two, three, four, five, six ->
                     one shouldBe "one"
                     two shouldBe "two"
                     three shouldBe "three"
@@ -67,12 +72,13 @@ class MultiLetSpec : WordSpec() {
                     six shouldBe "six"
 
                     Hexa(one, two, three, four, five, six)
-                } shouldBe Hexa("one", "two", "three", "four", "five", "six")
+                } shouldBe testData
             }
 
             "produce a correct binding from Hepta" {
+                val testData = Hepta("one", "two", "three", "four", "five", "six", "seven")
 
-                Hepta("one", "two", "three", "four", "five", "six", "seven").let { one, two, three, four, five, six, seven ->
+                testData.let { one, two, three, four, five, six, seven ->
                     one shouldBe "one"
                     two shouldBe "two"
                     three shouldBe "three"
@@ -82,24 +88,25 @@ class MultiLetSpec : WordSpec() {
                     seven shouldBe "seven"
 
                     Hepta(one, two, three, four, five, six, seven)
-                } shouldBe Hepta("one", "two", "three", "four", "five", "six", "seven")
+                } shouldBe testData
             }
 
             "handle null values" {
+                val actual = Pair<String?, String?>("one", null)
+                val expected = Pair("one", null)
 
-                Pair<String?, String?>("one", null).let { one, two ->
+                actual.let { one, two ->
                     one shouldBe "one"
                     two shouldBe null
 
                     Pair(one, two)
-                } shouldBe Pair("one", null)
+                } shouldBe expected
             }
 
             "not effect kotlin let" {
+                val actual = "something"
 
-                "something".let { someThingToBind ->
-                    "$someThingToBind to bind"
-                } shouldBe "something to bind"
+                actual.let { it } shouldBe "something"
             }
         }
     }
