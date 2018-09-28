3f101e7f36b133531587971c7a9c5a0c89a1c289
Benny Schneider <benny.schneider@rewe-digital.com>
2018-09-28 14:06:46 +0200

    Simplify MultiMapNotNull tests

--- multi-functions/src/test/kotlin/io/multifunctions/MultiMapNotNullSpec.kt
+++ multi-functions/src/test/kotlin/io/multifunctions/MultiMapNotNullSpec.kt
@@ -11,41 +11,45 @@ class MultiMapNotNullSpec : WordSpec() {
         "MultiMapNotNull" should {
 
             "produce a correct mapping from Pair" {
+                val testData = listOf(Pair("one", "two"))
 
-                listOf(Pair("one", "two")).mapNotNull { one, two ->
+                testData.mapNotNull { one, two ->
                     one shouldBe "one"
                     two shouldBe "two"
 
                     Pair(one, two)
-                } shouldBe listOf(Pair("one", "two"))
+                } shouldBe testData
             }
 
             "produce a correct mapping from Triple" {
+                val testData = listOf(Triple("one", "two", "three"))
 
-                listOf(Triple("one", "two", "three")).mapNotNull { one, two, three ->
+                testData.mapNotNull { one, two, three ->
                     one shouldBe "one"
                     two shouldBe "two"
                     three shouldBe "three"
 
                     Triple(one, two, three)
-                } shouldBe listOf(Triple("one", "two", "three"))
+                } shouldBe testData
             }
 
             "produce a correct mapping from Quad" {
+                val testData = listOf(Quad("one", "two", "three", "four"))
 
-                listOf(Quad("one", "two", "three", "four")).mapNotNull { one, two, three, four ->
+                testData.mapNotNull { one, two, three, four ->
                     one shouldBe "one"
                     two shouldBe "two"
                     three shouldBe "three"
                     four shouldBe "four"
 
                     Quad(one, two, three, four)
-                } shouldBe listOf(Quad("one", "two", "three", "four"))
+                } shouldBe testData
             }
 
             "produce a correct mapping from Penta" {
+                val testData = listOf(Penta("one", "two", "three", "four", "five"))
 
-                listOf(Penta("one", "two", "three", "four", "five")).mapNotNull { one, two, three, four, five ->
+                testData.mapNotNull { one, two, three, four, five ->
                     one shouldBe "one"
                     two shouldBe "two"
                     three shouldBe "three"
@@ -53,12 +57,13 @@ class MultiMapNotNullSpec : WordSpec() {
                     five shouldBe "five"
 
                     Penta(one, two, three, four, five)
-                } shouldBe listOf(Penta("one", "two", "three", "four", "five"))
+                } shouldBe testData
             }
 
             "produce a correct mapping from Hexa" {
+                val testData = listOf(Hexa("one", "two", "three", "four", "five", "six"))
 
-                listOf(Hexa("one", "two", "three", "four", "five", "six")).mapNotNull { one, two, three, four, five, six ->
+                testData.mapNotNull { one, two, three, four, five, six ->
                     one shouldBe "one"
                     two shouldBe "two"
                     three shouldBe "three"
@@ -67,12 +72,13 @@ class MultiMapNotNullSpec : WordSpec() {
                     six shouldBe "six"
 
                     Hexa(one, two, three, four, five, six)
-                } shouldBe listOf(Hexa("one", "two", "three", "four", "five", "six"))
+                } shouldBe testData
             }
 
             "produce a correct mapping from Hepta" {
+                val testData = listOf(Hepta("one", "two", "three", "four", "five", "six", "seven"))
 
-                listOf(Hepta("one", "two", "three", "four", "five", "six", "seven")).mapNotNull { one, two, three, four, five, six, seven ->
+                testData.mapNotNull { one, two, three, four, five, six, seven ->
                     one shouldBe "one"
                     two shouldBe "two"
                     three shouldBe "three"
@@ -82,7 +88,7 @@ class MultiMapNotNullSpec : WordSpec() {
                     seven shouldBe "seven"
 
                     Hepta(one, two, three, four, five, six, seven)
-                } shouldBe listOf(Hepta("one", "two", "three", "four", "five", "six", "seven"))
+                } shouldBe testData
             }
 
             "sort out null elements" {
