ae1a7e068b5abbb71933819ac6dc32563e91a1d4
Benny Schneider <stupacki86@gmail.com>
2019-05-12 22:10:26 +0200

    Make multi let check null infixable

--- multi-functions/src/test/kotlin/io/multifunctions/MultiLetCheckNullSpec.kt
+++ multi-functions/src/test/kotlin/io/multifunctions/MultiLetCheckNullSpec.kt
@@ -13,7 +13,7 @@ class MultiLetCheckNullSpec : WordSpec() {
             "produce a correct binding from Pair" {
                 val testData = Pair("one", "two")
 
-                testData.letCheckNull { one, two ->
+                testData letCheckNull { one, two ->
                     one shouldBe "one"
                     two shouldBe "two"
 
@@ -24,7 +24,7 @@ class MultiLetCheckNullSpec : WordSpec() {
             "produce a correct binding from Triple" {
                 val testData = Triple("one", "two", "three")
 
-                testData.letCheckNull { one, two, three ->
+                testData letCheckNull { one, two, three ->
                     one shouldBe "one"
                     two shouldBe "two"
                     three shouldBe "three"
@@ -36,7 +36,7 @@ class MultiLetCheckNullSpec : WordSpec() {
             "produce a correct binding from Quad" {
                 val testData = Quad("one", "two", "three", "four")
 
-                testData.letCheckNull { one, two, three, four ->
+                testData letCheckNull { one, two, three, four ->
                     one shouldBe "one"
                     two shouldBe "two"
                     three shouldBe "three"
@@ -49,7 +49,7 @@ class MultiLetCheckNullSpec : WordSpec() {
             "produce a correct binding from Penta" {
                 val testData = Penta("one", "two", "three", "four", "five")
 
-                testData.letCheckNull { one, two, three, four, five ->
+                testData letCheckNull { one, two, three, four, five ->
                     one shouldBe "one"
                     two shouldBe "two"
                     three shouldBe "three"
@@ -63,7 +63,7 @@ class MultiLetCheckNullSpec : WordSpec() {
             "produce a correct binding from Hexa" {
                 val testData = Hexa("one", "two", "three", "four", "five", "six")
 
-                testData.letCheckNull { one, two, three, four, five, six ->
+                testData letCheckNull { one, two, three, four, five, six ->
                     one shouldBe "one"
                     two shouldBe "two"
                     three shouldBe "three"
@@ -78,7 +78,7 @@ class MultiLetCheckNullSpec : WordSpec() {
             "produce a correct binding from Hepta" {
                 val testData = Hepta("one", "two", "three", "four", "five", "six", "seven")
 
-                testData.letCheckNull { one, two, three, four, five, six, seven ->
+                testData letCheckNull { one, two, three, four, five, six, seven ->
                     one shouldBe "one"
                     two shouldBe "two"
                     three shouldBe "three"
@@ -94,7 +94,7 @@ class MultiLetCheckNullSpec : WordSpec() {
             "handle null values" {
                 val actual = Pair<String?, String?>("one", null)
 
-                actual.letCheckNull { one, two ->
+                actual letCheckNull { one, two ->
                     one shouldBe "one"
                     two shouldBe null
 
