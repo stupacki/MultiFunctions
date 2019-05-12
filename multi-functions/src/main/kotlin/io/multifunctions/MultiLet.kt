efd7bf8dfa705427446fcbb049cb7cb72e480f7f
Benny Schneider <stupacki86@gmail.com>
2019-05-12 22:09:51 +0200

    Make multi let infixable

--- multi-functions/src/main/kotlin/io/multifunctions/MultiLet.kt
+++ multi-functions/src/main/kotlin/io/multifunctions/MultiLet.kt
@@ -2,14 +2,14 @@ package io.multifunctions
 
 import io.multifunctions.models.*
 
-inline fun <A, B, R> Pair<A?, B?>.let(transform: (A?, B?) -> R): R? = transform(first, second)
+inline infix fun <A, B, R> Pair<A?, B?>.let(transform: (A?, B?) -> R): R? = transform(first, second)
 
-inline fun <A, B, C, R> Triple<A?, B?, C?>.let(transform: (A?, B?, C?) -> R): R? = transform(first, second, third)
+inline infix fun <A, B, C, R> Triple<A?, B?, C?>.let(transform: (A?, B?, C?) -> R): R? = transform(first, second, third)
 
-inline fun <A, B, C, D, R> Quad<A?, B?, C?, D?>.let(transform: (A?, B?, C?, D?) -> R): R? = transform(first, second, third, fourth)
+inline infix fun <A, B, C, D, R> Quad<A?, B?, C?, D?>.let(transform: (A?, B?, C?, D?) -> R): R? = transform(first, second, third, fourth)
 
-inline fun <A, B, C, D, E, R> Penta<A?, B?, C?, D?, E?>.let(transform: (A?, B?, C?, D?, E?) -> R): R? = transform(first, second, third, fourth, fifth)
+inline infix fun <A, B, C, D, E, R> Penta<A?, B?, C?, D?, E?>.let(transform: (A?, B?, C?, D?, E?) -> R): R? = transform(first, second, third, fourth, fifth)
 
-inline fun <A, B, C, D, E, F, R> Hexa<A?, B?, C?, D?, E?, F?>.let(transform: (A?, B?, C?, D?, E?, F?) -> R): R? = transform(first, second, third, fourth, fifth, sixth)
+inline infix fun <A, B, C, D, E, F, R> Hexa<A?, B?, C?, D?, E?, F?>.let(transform: (A?, B?, C?, D?, E?, F?) -> R): R? = transform(first, second, third, fourth, fifth, sixth)
 
-inline fun <A, B, C, D, E, F, G, R> Hepta<A?, B?, C?, D?, E?, F?, G?>.let(transform: (A?, B?, C?, D?, E?, F?, G?) -> R): R? = transform(first, second, third, fourth, fifth, sixth, seventh)
+inline infix fun <A, B, C, D, E, F, G, R> Hepta<A?, B?, C?, D?, E?, F?, G?>.let(transform: (A?, B?, C?, D?, E?, F?, G?) -> R): R? = transform(first, second, third, fourth, fifth, sixth, seventh)
--- multi-functions/src/test/kotlin/io/multifunctions/MultiLetSpec.kt
+++ multi-functions/src/test/kotlin/io/multifunctions/MultiLetSpec.kt
@@ -13,7 +13,7 @@ class MultiLetSpec : WordSpec() {
             "produce a correct binding from Pair" {
                 val testData = Pair("one", "two")
 
-                testData.let { one, two ->
+                testData let { one, two ->
                     one shouldBe "one"
                     two shouldBe "two"
 
@@ -24,7 +24,7 @@ class MultiLetSpec : WordSpec() {
             "produce a correct binding from Triple" {
                 val testData = Triple("one", "two", "three")
 
-                testData.let { one, two, three ->
+                testData let { one, two, three ->
                     one shouldBe "one"
                     two shouldBe "two"
                     three shouldBe "three"
@@ -36,7 +36,7 @@ class MultiLetSpec : WordSpec() {
             "produce a correct binding from Quad" {
                 val testData = Quad("one", "two", "three", "four")
 
-                testData.let { one, two, three, four ->
+                testData let { one, two, three, four ->
                     one shouldBe "one"
                     two shouldBe "two"
                     three shouldBe "three"
@@ -49,7 +49,7 @@ class MultiLetSpec : WordSpec() {
             "produce a correct binding from Penta" {
                 val testData = Penta("one", "two", "three", "four", "five")
 
-                testData.let { one, two, three, four, five ->
+                testData let { one, two, three, four, five ->
                     one shouldBe "one"
                     two shouldBe "two"
                     three shouldBe "three"
@@ -63,7 +63,7 @@ class MultiLetSpec : WordSpec() {
             "produce a correct binding from Hexa" {
                 val testData = Hexa("one", "two", "three", "four", "five", "six")
 
-                testData.let { one, two, three, four, five, six ->
+                testData let { one, two, three, four, five, six ->
                     one shouldBe "one"
                     two shouldBe "two"
                     three shouldBe "three"
@@ -78,7 +78,7 @@ class MultiLetSpec : WordSpec() {
             "produce a correct binding from Hepta" {
                 val testData = Hepta("one", "two", "three", "four", "five", "six", "seven")
 
-                testData.let { one, two, three, four, five, six, seven ->
+                testData let { one, two, three, four, five, six, seven ->
                     one shouldBe "one"
                     two shouldBe "two"
                     three shouldBe "three"
@@ -95,7 +95,7 @@ class MultiLetSpec : WordSpec() {
                 val actual = Pair<String?, String?>("one", null)
                 val expected = Pair("one", null)
 
-                actual.let { one, two ->
+                actual let { one, two ->
                     one shouldBe "one"
                     two shouldBe null
 
