6ec4128742df7ed52be875ac75b3667863627a40
Benny Schneider <stupacki86@gmail.com>
2019-05-18 14:45:40 +0200

    Add docu on multi for each

--- multi-functions/src/main/kotlin/io/multifunctions/MultiForEach.kt
+++ multi-functions/src/main/kotlin/io/multifunctions/MultiForEach.kt
@@ -5,31 +5,49 @@ import io.multifunctions.models.Hexa
 import io.multifunctions.models.Penta
 import io.multifunctions.models.Quad
 
+/**
+ * Performs the given [action] on each [Pair] element.
+ */
 inline infix fun <A, B> Iterable<Pair<A?, B?>>.forEach(action: (A?, B?) -> Unit) =
         forEach { (first, second) ->
             action(first, second)
         }
 
+/**
+ * Performs the given [action] on each [Triple] element.
+ */
 inline infix fun <A, B, C> Iterable<Triple<A?, B?, C?>>.forEach(action: (A?, B?, C?) -> Unit) =
         forEach { (first, second, third) ->
             action(first, second, third)
         }
 
+/**
+ * Performs the given [action] on each [Quad] element.
+ */
 inline infix fun <A, B, C, D> Iterable<Quad<A?, B?, C?, D?>>.forEach(action: (A?, B?, C?, D?) -> Unit) =
         forEach { (first, second, third, fourth) ->
             action(first, second, third, fourth)
         }
 
+/**
+ * Performs the given [action] on each [Penta] element.
+ */
 inline infix fun <A, B, C, D, E> Iterable<Penta<A?, B?, C?, D?, E?>>.forEach(action: (A?, B?, C?, D?, E?) -> Unit) =
         forEach { (first, second, third, fourth, fifth) ->
             action(first, second, third, fourth, fifth)
         }
 
+/**
+ * Performs the given [action] on each [Hexa] element.
+ */
 inline infix fun <A, B, C, D, E, F> Iterable<Hexa<A?, B?, C?, D?, E?, F?>>.forEach(action: (A?, B?, C?, D?, E?, F?) -> Unit) =
         forEach { (first, second, third, fourth, fifth, sixth) ->
             action(first, second, third, fourth, fifth, sixth)
         }
 
+/**
+ * Performs the given [action] on each [Hepta] element.
+ */
 inline infix fun <A, B, C, D, E, F, G> Iterable<Hepta<A?, B?, C?, D?, E?, F?, G?>>.forEach(action: (A?, B?, C?, D?, E?, F?, G?) -> Unit) =
         forEach { (first, second, third, fourth, fifth, sixth, seventh) ->
             action(first, second, third, fourth, fifth, sixth, seventh)
