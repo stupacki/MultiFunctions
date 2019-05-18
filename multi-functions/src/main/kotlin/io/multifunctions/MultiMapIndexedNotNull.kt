0b69363f852622fc62bd8f5e349422cfc0aec9ef
Benny Schneider <stupacki86@gmail.com>
2019-05-18 16:01:42 +0200

    Optimize code for mapNotNull and mapIndexedNotNull

--- multi-functions/src/main/kotlin/io/multifunctions/MultiMapIndexedNotNull.kt
+++ multi-functions/src/main/kotlin/io/multifunctions/MultiMapIndexedNotNull.kt
@@ -6,49 +6,49 @@ import io.multifunctions.models.Penta
 import io.multifunctions.models.Quad
 
 inline infix fun <A, B, R : Any> Iterable<Pair<A?, B?>>.mapIndexedNotNull(transform: (Int, A?, B?) -> R?): List<R> =
-        mapIndexedNotNull { index, (first, second) ->
-            when (Pair(first, second)) {
-                Pair(null, null) -> null
-                else -> transform(index, first, second)
-            }
+    mapIndexedNotNull { index, (first, second) ->
+        when {
+            first == null && second == null -> null
+            else -> transform(index, first, second)
         }
+    }
 
 inline infix fun <A, B, C, R : Any> Iterable<Triple<A?, B?, C?>>.mapIndexedNotNull(transform: (Int, A?, B?, C?) -> R?): List<R> =
-        mapIndexedNotNull { index, (first, second, third) ->
-            when (Triple(first, second, third)) {
-                Triple(null, null, null) -> null
-                else -> transform(index, first, second, third)
-            }
+    mapIndexedNotNull { index, (first, second, third) ->
+        when {
+            first == null && second == null && third == null -> null
+            else -> transform(index, first, second, third)
         }
+    }
 
 inline infix fun <A, B, C, D, R : Any> Iterable<Quad<A?, B?, C?, D?>>.mapIndexedNotNull(transform: (Int, A?, B?, C?, D?) -> R?): List<R> =
-        mapIndexedNotNull { index, (first, second, third, fourth) ->
-            when (Quad(first, second, third, fourth)) {
-                Quad(null, null, null, null) -> null
-                else -> transform(index, first, second, third, fourth)
-            }
+    mapIndexedNotNull { index, (first, second, third, fourth) ->
+        when {
+            first == null && second == null && third == null && fourth == null -> null
+            else -> transform(index, first, second, third, fourth)
         }
+    }
 
 inline infix fun <A, B, C, D, E, R : Any> Iterable<Penta<A?, B?, C?, D?, E?>>.mapIndexedNotNull(transform: (Int, A?, B?, C?, D?, E?) -> R?): List<R> =
-        mapIndexedNotNull { index, (first, second, third, fourth, fifth) ->
-            when (Penta(first, second, third, fourth, fifth)) {
-                Penta(null, null, null, null, null) -> null
-                else -> transform(index, first, second, third, fourth, fifth)
-            }
+    mapIndexedNotNull { index, (first, second, third, fourth, fifth) ->
+        when {
+            first == null && second == null && third == null && fourth == null && fifth == null -> null
+            else -> transform(index, first, second, third, fourth, fifth)
         }
+    }
 
 inline infix fun <A, B, C, D, E, F, R : Any> Iterable<Hexa<A?, B?, C?, D?, E?, F?>>.mapIndexedNotNull(transform: (Int, A?, B?, C?, D?, E?, F?) -> R?): List<R> =
-        mapIndexedNotNull { index, (first, second, third, fourth, fifth, sixth) ->
-            when (Hexa(first, second, third, fourth, fifth, sixth)) {
-                Hexa(null, null, null, null, null, null) -> null
-                else -> transform(index, first, second, third, fourth, fifth, sixth)
-            }
+    mapIndexedNotNull { index, (first, second, third, fourth, fifth, sixth) ->
+        when {
+            first == null && second == null && third == null && fourth == null && fifth == null && sixth == null -> null
+            else -> transform(index, first, second, third, fourth, fifth, sixth)
         }
+    }
 
 inline infix fun <A, B, C, D, E, F, G, R : Any> Iterable<Hepta<A?, B?, C?, D?, E?, F?, G?>>.mapIndexedNotNull(transform: (Int, A?, B?, C?, D?, E?, F?, G?) -> R?): List<R> =
-        mapIndexedNotNull { index, (first, second, third, fourth, fifth, sixth, seventh) ->
-            when (Hepta(first, second, third, fourth, fifth, sixth, seventh)) {
-                Hepta(null, null, null, null, null, null, null) -> null
-                else -> transform(index, first, second, third, fourth, fifth, sixth, seventh)
-            }
+    mapIndexedNotNull { index, (first, second, third, fourth, fifth, sixth, seventh) ->
+        when {
+            first == null && second == null && third == null && fourth == null && fifth == null && sixth == null && seventh == null -> null
+            else -> transform(index, first, second, third, fourth, fifth, sixth, seventh)
         }
+    }
--- multi-functions/src/main/kotlin/io/multifunctions/MultiMapNotNull.kt
+++ multi-functions/src/main/kotlin/io/multifunctions/MultiMapNotNull.kt
@@ -6,49 +6,49 @@ import io.multifunctions.models.Penta
 import io.multifunctions.models.Quad
 
 inline infix fun <A, B, R : Any> Iterable<Pair<A?, B?>>.mapNotNull(transform: (A?, B?) -> R?): List<R> =
-        mapNotNull { (first, second) ->
-            when (Pair(first, second)) {
-                Pair(null, null) -> null
-                else -> transform(first, second)
-            }
+    mapNotNull { (first, second) ->
+        when {
+            first == null && second == null -> null
+            else -> transform(first, second)
         }
+    }
 
 inline infix fun <A, B, C, R : Any> Iterable<Triple<A?, B?, C?>>.mapNotNull(transform: (A?, B?, C?) -> R?): List<R> =
-        mapNotNull { (first, second, third) ->
-            when (Triple(first, second, third)) {
-                Triple(null, null, null) -> null
-                else -> transform(first, second, third)
-            }
+    mapNotNull { (first, second, third) ->
+        when {
+            first == null && second == null && third == null -> null
+            else -> transform(first, second, third)
         }
+    }
 
 inline infix fun <A, B, C, D, R : Any> Iterable<Quad<A?, B?, C?, D?>>.mapNotNull(transform: (A?, B?, C?, D?) -> R?): List<R> =
-        mapNotNull { (first, second, third, fourth) ->
-            when (Quad(first, second, third, fourth)) {
-                Quad(null, null, null, null) -> null
-                else -> transform(first, second, third, fourth)
-            }
+    mapNotNull { (first, second, third, fourth) ->
+        when {
+            first == null && second == null && third == null && fourth == null -> null
+            else -> transform(first, second, third, fourth)
         }
+    }
 
 inline infix fun <A, B, C, D, E, R : Any> Iterable<Penta<A?, B?, C?, D?, E?>>.mapNotNull(transform: (A?, B?, C?, D?, E?) -> R?): List<R> =
-        mapNotNull { (first, second, third, fourth, fifth) ->
-            when (Penta(first, second, third, fourth, fifth)) {
-                Quad(null, null, null, null) -> null
-                else -> transform(first, second, third, fourth, fifth)
-            }
+    mapNotNull { (first, second, third, fourth, fifth) ->
+        when {
+            first == null && second == null && third == null && fourth == null && fifth == null -> null
+            else -> transform(first, second, third, fourth, fifth)
         }
+    }
 
 inline infix fun <A, B, C, D, E, F, R : Any> Iterable<Hexa<A?, B?, C?, D?, E?, F?>>.mapNotNull(transform: (A?, B?, C?, D?, E?, F?) -> R?): List<R> =
-        mapNotNull { (first, second, third, fourth, fifth, sixth) ->
-            when (Hexa(first, second, third, fourth, fifth, sixth)) {
-                Hexa(null, null, null, null, null, null) -> null
-                else -> transform(first, second, third, fourth, fifth, sixth)
-            }
+    mapNotNull { (first, second, third, fourth, fifth, sixth) ->
+        when {
+            first == null && second == null && third == null && fourth == null && fifth == null && sixth == null -> null
+            else -> transform(first, second, third, fourth, fifth, sixth)
         }
+    }
 
 inline infix fun <A, B, C, D, E, F, G, R : Any> Iterable<Hepta<A?, B?, C?, D?, E?, F?, G?>>.mapNotNull(transform: (A?, B?, C?, D?, E?, F?, G?) -> R?): List<R> =
-        mapNotNull { (first, second, third, fourth, fifth, sixth, seventh) ->
-            when (Hepta(first, second, third, fourth, fifth, sixth, seventh)) {
-                Hexa(null, null, null, null, null, null) -> null
-                else -> transform(first, second, third, fourth, fifth, sixth, seventh)
-            }
+    mapNotNull { (first, second, third, fourth, fifth, sixth, seventh) ->
+        when {
+            first == null && second == null && third == null && fourth == null && fifth == null && sixth == null && seventh == null -> null
+            else -> transform(first, second, third, fourth, fifth, sixth, seventh)
         }
+    }
