bb80bb8c3fe390336786e58170c816458d109962
Benny Schneider <stupacki86@gmail.com>
2019-05-18 14:52:45 +0200

    Use block instead of transform fo multi let

--- multi-functions/src/main/kotlin/io/multifunctions/MultiLet.kt
+++ multi-functions/src/main/kotlin/io/multifunctions/MultiLet.kt
@@ -8,35 +8,35 @@ import io.multifunctions.models.Quad
 /**
  * Calls the specified function [block] with `this` value as its arguments and returns its result.
  */
-inline infix fun <A, B, R> Pair<A?, B?>.let(transform: (A?, B?) -> R): R? =
-        transform(first, second)
+inline infix fun <A, B, R> Pair<A?, B?>.let(blockg: (A?, B?) -> R): R? =
+        block(first, second)
 
 /**
  * Calls the specified function [block] with `this` value as its arguments and returns its result.
  */
-inline infix fun <A, B, C, R> Triple<A?, B?, C?>.let(transform: (A?, B?, C?) -> R): R? =
-        transform(first, second, third)
+inline infix fun <A, B, C, R> Triple<A?, B?, C?>.let(block: (A?, B?, C?) -> R): R? =
+        block(first, second, third)
 
 /**
  * Calls the specified function [block] with `this` value as its arguments and returns its result.
  */
-inline infix fun <A, B, C, D, R> Quad<A?, B?, C?, D?>.let(transform: (A?, B?, C?, D?) -> R): R? =
-        transform(first, second, third, fourth)
+inline infix fun <A, B, C, D, R> Quad<A?, B?, C?, D?>.let(block: (A?, B?, C?, D?) -> R): R? =
+        block(first, second, third, fourth)
 
 /**
  * Calls the specified function [block] with `this` value as its arguments and returns its result.
  */
-inline infix fun <A, B, C, D, E, R> Penta<A?, B?, C?, D?, E?>.let(transform: (A?, B?, C?, D?, E?) -> R): R? =
-        transform(first, second, third, fourth, fifth)
+inline infix fun <A, B, C, D, E, R> Penta<A?, B?, C?, D?, E?>.let(block: (A?, B?, C?, D?, E?) -> R): R? =
+        block(first, second, third, fourth, fifth)
 
 /**
  * Calls the specified function [block] with `this` value as its arguments and returns its result.
  */
-inline infix fun <A, B, C, D, E, F, R> Hexa<A?, B?, C?, D?, E?, F?>.let(transform: (A?, B?, C?, D?, E?, F?) -> R): R? =
-        transform(first, second, third, fourth, fifth, sixth)
+inline infix fun <A, B, C, D, E, F, R> Hexa<A?, B?, C?, D?, E?, F?>.let(block: (A?, B?, C?, D?, E?, F?) -> R): R? =
+        block(first, second, third, fourth, fifth, sixth)
 
 /**
  * Calls the specified function [block] with `this` value as its arguments and returns its result.
  */
-inline infix fun <A, B, C, D, E, F, G, R> Hepta<A?, B?, C?, D?, E?, F?, G?>.let(transform: (A?, B?, C?, D?, E?, F?, G?) -> R): R? =
-        transform(first, second, third, fourth, fifth, sixth, seventh)
+inline infix fun <A, B, C, D, E, F, G, R> Hepta<A?, B?, C?, D?, E?, F?, G?>.let(block: (A?, B?, C?, D?, E?, F?, G?) -> R): R? =
+        block(first, second, third, fourth, fifth, sixth, seventh)
