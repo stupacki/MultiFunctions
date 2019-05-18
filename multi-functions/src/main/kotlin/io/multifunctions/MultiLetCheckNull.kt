28a2955751c212058a0417043fc333cfa434124b
Benny Schneider <stupacki86@gmail.com>
2019-05-18 14:52:13 +0200

    Use block instead of transform fo multi let not null

--- multi-functions/src/main/kotlin/io/multifunctions/MultiLetCheckNull.kt
+++ multi-functions/src/main/kotlin/io/multifunctions/MultiLetCheckNull.kt
@@ -10,53 +10,53 @@ import io.multifunctions.models.Quad
 /**
  * Calls the specified function [block] with `this` value as its arguments and returns its result when no element is [null].
  */
-inline infix fun <A, B, R> Pair<A?, B?>.letCheckNull(transform: (A, B) -> R): R? =
+inline infix fun <A, B, R> Pair<A?, B?>.letCheckNull(block: (A, B) -> R): R? =
         when (null) {
             first, second -> null
-            else -> transform(first as A, second as B)
+            else -> block(first as A, second as B)
         }
 
 /**
  * Calls the specified function [block] with `this` value as its arguments and returns its result when no element is [null].
  */
-inline infix fun <A, B, C, R> Triple<A?, B?, C?>.letCheckNull(transform: (A, B, C) -> R): R? =
+inline infix fun <A, B, C, R> Triple<A?, B?, C?>.letCheckNull(block: (A, B, C) -> R): R? =
         when (null) {
             first, second, third -> null
-            else -> transform(first as A, second as B, third as C)
+            else -> block(first as A, second as B, third as C)
         }
 
 /**
  * Calls the specified function [block] with `this` value as its arguments and returns its result when no element is [null].
  */
-inline infix fun <A, B, C, D, R> Quad<A?, B?, C?, D?>.letCheckNull(transform: (A, B, C, D) -> R): R? =
+inline infix fun <A, B, C, D, R> Quad<A?, B?, C?, D?>.letCheckNull(block: (A, B, C, D) -> R): R? =
         when (null) {
             first, second, third, fourth -> null
-            else -> transform(first, second, third, fourth)
+            else -> block(first, second, third, fourth)
         }
 
 /**
  * Calls the specified function [block] with `this` value as its arguments and returns its result when no element is [null].
  */
-inline infix fun <A, B, C, D, E, R> Penta<A?, B?, C?, D?, E?>.letCheckNull(transform: (A, B, C, D, E) -> R): R? =
+inline infix fun <A, B, C, D, E, R> Penta<A?, B?, C?, D?, E?>.letCheckNull(block: (A, B, C, D, E) -> R): R? =
         when (null) {
             first, second, third, fourth, fifth -> null
-            else -> transform(first, second, third, fourth, fifth)
+            else -> block(first, second, third, fourth, fifth)
         }
 
 /**
  * Calls the specified function [block] with `this` value as its arguments and returns its result when no element is [null].
  */
-inline infix fun <A, B, C, D, E, F, R> Hexa<A?, B?, C?, D?, E?, F?>.letCheckNull(transform: (A, B, C, D, E, F) -> R): R? =
+inline infix fun <A, B, C, D, E, F, R> Hexa<A?, B?, C?, D?, E?, F?>.letCheckNull(block: (A, B, C, D, E, F) -> R): R? =
         when (null) {
             first, second, third, fourth, fifth, sixth -> null
-            else -> transform(first, second, third, fourth, fifth, sixth)
+            else -> block(first, second, third, fourth, fifth, sixth)
         }
 
 /**
  * Calls the specified function [block] with `this` value as its arguments and returns its result when no element is [null].
  */
-inline infix fun <A, B, C, D, E, F, G, R> Hepta<A?, B?, C?, D?, E?, F?, G?>.letCheckNull(transform: (A, B, C, D, E, F, G) -> R): R? =
+inline infix fun <A, B, C, D, E, F, G, R> Hepta<A?, B?, C?, D?, E?, F?, G?>.letCheckNull(block: (A, B, C, D, E, F, G) -> R): R? =
         when (null) {
             first, second, third, fourth, fifth, sixth, seventh -> null
-            else -> transform(first, second, third, fourth, fifth, sixth, seventh)
+            else -> block(first, second, third, fourth, fifth, sixth, seventh)
         }
