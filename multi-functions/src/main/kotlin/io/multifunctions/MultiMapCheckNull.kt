36d35b2ae051c48dbed8d895c6823f842dc5a6d4
Benny Schneider <stupacki86@gmail.com>
2019-05-18 15:04:23 +0200

    Add docu to multi let check null

--- multi-functions/src/main/kotlin/io/multifunctions/MultiMapCheckNull.kt
+++ multi-functions/src/main/kotlin/io/multifunctions/MultiMapCheckNull.kt
@@ -5,6 +5,11 @@ import io.multifunctions.models.Hexa
 import io.multifunctions.models.Penta
 import io.multifunctions.models.Quad
 
+/**
+ * Returns a list containing only the non-null results of applying the given [transform] function
+ * to each element in the original collection. When one parameter of the tuple is null the element
+ * will be removed from the collection.
+ */
 inline infix fun <A, B, R : Any> Iterable<Pair<A?, B?>>.mapCheckNull(transform: (A, B) -> R): List<R> =
         mapNotNull { (first, second) ->
             when (null) {
@@ -13,6 +18,11 @@ inline infix fun <A, B, R : Any> Iterable<Pair<A?, B?>>.mapCheckNull(transform: 
             }
         }
 
+/**
+ * Returns a list containing only the non-null results of applying the given [transform] function
+ * to each element in the original collection. When one parameter of the tuple is null the element
+ * will be removed from the collection.
+ */
 inline infix fun <A, B, C, R : Any> Iterable<Triple<A?, B?, C?>>.mapCheckNull(transform: (A, B, C) -> R): List<R> =
         mapNotNull { (first, second, third) ->
             when (null) {
@@ -21,6 +31,11 @@ inline infix fun <A, B, C, R : Any> Iterable<Triple<A?, B?, C?>>.mapCheckNull(tr
             }
         }
 
+/**
+ * Returns a list containing only the non-null results of applying the given [transform] function
+ * to each element in the original collection. When one parameter of the tuple is null the element
+ * will be removed from the collection.
+ */
 inline infix fun <A, B, C, D, R : Any> Iterable<Quad<A?, B?, C?, D?>>.mapCheckNull(transform: (A, B, C, D) -> R): List<R> =
         mapNotNull { (first, second, third, fourth) ->
             when (null) {
@@ -29,6 +44,11 @@ inline infix fun <A, B, C, D, R : Any> Iterable<Quad<A?, B?, C?, D?>>.mapCheckNu
             }
         }
 
+/**
+ * Returns a list containing only the non-null results of applying the given [transform] function
+ * to each element in the original collection. When one parameter of the tuple is null the element
+ * will be removed from the collection.
+ */
 inline infix fun <A, B, C, D, E, R : Any> Iterable<Penta<A?, B?, C?, D?, E?>>.mapCheckNull(transform: (A, B, C, D, E) -> R): List<R> =
         mapNotNull { (first, second, third, fourth, fifth) ->
             when (null) {
@@ -37,6 +57,11 @@ inline infix fun <A, B, C, D, E, R : Any> Iterable<Penta<A?, B?, C?, D?, E?>>.ma
             }
         }
 
+/**
+ * Returns a list containing only the non-null results of applying the given [transform] function
+ * to each element in the original collection. When one parameter of the tuple is null the element
+ * will be removed from the collection.
+ */
 inline infix fun <A, B, C, D, E, F, R : Any> Iterable<Hexa<A?, B?, C?, D?, E?, F?>>.mapCheckNull(transform: (A, B, C, D, E, F) -> R): List<R> =
         mapNotNull { (first, second, third, fourth, fifth, sixth) ->
             when (null) {
@@ -45,6 +70,11 @@ inline infix fun <A, B, C, D, E, F, R : Any> Iterable<Hexa<A?, B?, C?, D?, E?, F
             }
         }
 
+/**
+ * Returns a list containing only the non-null results of applying the given [transform] function
+ * to each element in the original collection. When one parameter of the tuple is null the element
+ * will be removed from the collection.
+ */
 inline infix fun <A, B, C, D, E, F, G, R : Any> Iterable<Hepta<A?, B?, C?, D?, E?, F?, G?>>.mapCheckNull(transform: (A, B, C, D, E, F, G) -> R): List<R> =
         mapNotNull { (first, second, third, fourth, fifth, sixth, seventh) ->
             when (null) {
