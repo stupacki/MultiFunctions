1dedebfe5833efa037db1338c59502a777165609
Benny Schneider <benny.schneider@Bennys-MBP.fritz.box>
2022-07-10 22:22:30 +0200

    Add a small info on joinToTuple functions

--- multi-functions/src/main/kotlin/io/multifunctions/JoinToTuple.kt
+++ multi-functions/src/main/kotlin/io/multifunctions/JoinToTuple.kt
@@ -5,12 +5,18 @@ import io.multifunctions.models.Hexa
 import io.multifunctions.models.Penta
 import io.multifunctions.models.Quad
 
+/**
+ * Joins [A] and [B] into a [Pair]
+ */
 fun <A, B> joinToTuple(
     first: A,
     second: B,
 ): Pair<A, B> =
     Pair(first, second)
 
+/**
+ * Joins [A], [B] and [C] into a [Triple]
+ */
 fun <A, B, C> joinToTuple(
     first: A,
     second: B,
@@ -18,6 +24,9 @@ fun <A, B, C> joinToTuple(
 ): Triple<A, B, C> =
     Triple(first, second, third)
 
+/**
+ * Joins [A], [B], [C] and [D] into a [Quad]
+ */
 fun <A, B, C, D> joinToTuple(
     first: A,
     second: B,
@@ -26,6 +35,9 @@ fun <A, B, C, D> joinToTuple(
 ): Quad<A, B, C, D> =
     Quad(first, second, third, fourth)
 
+/**
+ * Joins [A], [B], [C], [D] and [E] into a [Penta]
+ */
 fun <A, B, C, D, E> joinToTuple(
     first: A,
     second: B,
@@ -35,6 +47,9 @@ fun <A, B, C, D, E> joinToTuple(
 ): Penta<A, B, C, D, E> =
     Penta(first, second, third, fourth, fifth)
 
+/**
+ * Joins [A], [B], [C], [D], [E] and [F] into a [Hexa]
+ */
 fun <A, B, C, D, E, F> joinToTuple(
     first: A,
     second: B,
@@ -45,6 +60,9 @@ fun <A, B, C, D, E, F> joinToTuple(
 ): Hexa<A, B, C, D, E, F> =
     Hexa(first, second, third, fourth, fifth, sixth)
 
+/**
+ * Joins [A], [B], [C], [D], [E], [F] and [G] into a [Hepta]
+ */
 fun <A, B, C, D, E, F, G> joinToTuple(
     first: A,
     second: B,
