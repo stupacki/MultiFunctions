b75d6e555af074fe7acb80869c3c75953a9e12b8
Benny Schneider <stupacki86@gmail.com>
2023-10-18 11:22:48 +0200

    Use reified params and inline functions

--- multi-functions/src/main/kotlin/io/multifunctions/NotNull.kt
+++ multi-functions/src/main/kotlin/io/multifunctions/NotNull.kt
@@ -8,16 +8,18 @@ import io.multifunctions.models.Quad
 /**
  * Calls the specified function [block] with [first] and [second] value as its arguments and returns its result when no element is [null].
  */
-fun <A, B, R> notNull(first: A?, second: B?, block: (A, B) -> R) =
+inline fun <reified A, reified B, R> notNull(first: A?, second: B?, block: (A, B) -> R) =
         when (null) {
             first, second -> null
-            else -> block(first as A, second as B)
+            else -> {
+                block(first as A, second as B)
+            }
         }
 
 /**
  * Calls the specified function [block] with [first], [second] and [third] value as its arguments and returns its result when no element is [null].
  */
-fun <A, B, C, R> notNull(first: A?, second: B?, third: C?, block: (A, B, C) -> R) =
+inline fun <reified A, reified B, reified C, R> notNull(first: A?, second: B?, third: C?, block: (A, B, C) -> R) =
         when (null) {
             first, second, third -> null
             else -> block(first as A, second as B, third as C)
@@ -26,7 +28,7 @@ fun <A, B, C, R> notNull(first: A?, second: B?, third: C?, block: (A, B, C) -> R
 /**
  * Calls the specified function [block] with [first], [second], [third] and [fourth] value as its arguments and returns its result when no element is [null].
  */
-fun <A, B, C, D, R> notNull(first: A?, second: B?, third: C?, fourth: D?, block: (A, B, C, D) -> R) =
+inline fun <reified A, reified B, reified C, reified D, R> notNull(first: A?, second: B?, third: C?, fourth: D?, block: (A, B, C, D) -> R) =
         when (null) {
             first, second, third, fourth -> null
             else -> block(first as A, second as B, third as C, fourth as D)
@@ -35,7 +37,7 @@ fun <A, B, C, D, R> notNull(first: A?, second: B?, third: C?, fourth: D?, block:
 /**
  * Calls the specified function [block] with [first], [second], [third], [fourth] and [fifth] value as its arguments and returns its result when no element is [null].
  */
-fun <A, B, C, D, E, R> notNull(first: A?, second: B?, third: C?, fourth: D?, fifth: E?, block: (A, B, C, D, E) -> R) =
+inline fun <reified A, reified B, reified C, reified D, reified E, R> notNull(first: A?, second: B?, third: C?, fourth: D?, fifth: E?, block: (A, B, C, D, E) -> R) =
         when (null) {
             first, second, third, fourth, fifth -> null
             else -> block(first as A, second as B, third as C, fourth as D, fifth as E)
@@ -44,7 +46,7 @@ fun <A, B, C, D, E, R> notNull(first: A?, second: B?, third: C?, fourth: D?, fif
 /**
  * Calls the specified function [block] with [first], [second], [third], [fourth], [fifth] and [sixth] value as its arguments and returns its result when no element is [null].
  */
-fun <A, B, C, D, E, F, R> notNull(first: A?, second: B?, third: C?, fourth: D?, fifth: E?, sixth: F?, block: (A, B, C, D, E, F) -> R) =
+inline fun <reified A, reified B, reified C, reified D, reified E, reified F, R> notNull(first: A?, second: B?, third: C?, fourth: D?, fifth: E?, sixth: F?, block: (A, B, C, D, E, F) -> R) =
         when (null) {
             first, second, third, fourth, fifth, sixth -> null
             else -> block(first as A, second as B, third as C, fourth as D, fifth as E, sixth as F)
@@ -53,7 +55,7 @@ fun <A, B, C, D, E, F, R> notNull(first: A?, second: B?, third: C?, fourth: D?, 
 /**
  * Calls the specified function [block] with [first], [second], [third], [fourth], [fifth], [sixth] and [seventh] value as its arguments and returns its result when no element is [null].
  */
-fun <A, B, C, D, E, F, G, R> notNull(first: A?, second: B?, third: C?, fourth: D?, fifth: E?, sixth: F?, seventh: G?, block: (A, B, C, D, E, F, G) -> R) =
+inline fun <reified A, reified B, reified C, reified D, reified E, reified F, reified G, R> notNull(first: A?, second: B?, third: C?, fourth: D?, fifth: E?, sixth: F?, seventh: G?, block: (A, B, C, D, E, F, G) -> R) =
         when (null) {
             first, second, third, fourth, fifth, sixth, seventh -> null
             else -> block(first as A, second as B, third as C, fourth as D, fifth as E, sixth as F, seventh as G)
