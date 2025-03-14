4d31c741990ce9f8f67b194b7bfaeb423c81b7c6
Benny Schneider <stupacki86@gmail.com>
2025-03-14 19:24:10 +0100

    Add java doc to flatMap

--- multi-functions/src/main/kotlin/io/multifunctions/MultiFlatMap.kt
+++ multi-functions/src/main/kotlin/io/multifunctions/MultiFlatMap.kt
@@ -6,7 +6,13 @@ import io.multifunctions.models.Penta
 import io.multifunctions.models.Quad
 
 /**
- * Returns a single list of all elements yielded from results of [transform] function being invoked on each [Pair] element of original collection.
+ * Returns a single list of all elements yielded from results of [transform] function being invoked on each [Pair] element of the original collection.
+ *
+ * @param A the type of the first value in the pair
+ * @param B the type of the second value in the pair
+ * @param R the type of the result elements
+ * @param transform the function to transform each pair into an iterable of results
+ * @return a list of all elements yielded from the transform function
  */
 inline fun <A, B, R> Iterable<Pair<A?, B?>>.flatMap(
     transform: (A?, B?) -> Iterable<R>
@@ -15,7 +21,14 @@ inline fun <A, B, R> Iterable<Pair<A?, B?>>.flatMap(
 }
 
 /**
- * Returns a single list of all elements yielded from results of [transform] function being invoked on each [Triple] element of original collection.
+ * Returns a single list of all elements yielded from results of [transform] function being invoked on each [Triple] element of the original collection.
+ *
+ * @param A the type of the first value in the triple
+ * @param B the type of the second value in the triple
+ * @param C the type of the third value in the triple
+ * @param R the type of the result elements
+ * @param transform the function to transform each triple into an iterable of results
+ * @return a list of all elements yielded from the transform function
  */
 inline fun <A, B, C, R> Iterable<Triple<A?, B?, C?>>.flatMap(
     transform: (A?, B?, C?) -> Iterable<R>
@@ -24,7 +37,15 @@ inline fun <A, B, C, R> Iterable<Triple<A?, B?, C?>>.flatMap(
 }
 
 /**
- * Returns a single list of all elements yielded from results of [transform] function being invoked on each [Quad] element of original collection.
+ * Returns a single list of all elements yielded from results of [transform] function being invoked on each [Quad] element of the original collection.
+ *
+ * @param A the type of the first value in the quad
+ * @param B the type of the second value in the quad
+ * @param C the type of the third value in the quad
+ * @param D the type of the fourth value in the quad
+ * @param R the type of the result elements
+ * @param transform the function to transform each quad into an iterable of results
+ * @return a list of all elements yielded from the transform function
  */
 inline fun <A, B, C, D, R> Iterable<Quad<A?, B?, C?, D?>>.flatMap(
     transform: (A?, B?, C?, D?) -> Iterable<R>
@@ -33,7 +54,16 @@ inline fun <A, B, C, D, R> Iterable<Quad<A?, B?, C?, D?>>.flatMap(
 }
 
 /**
- * Returns a single list of all elements yielded from results of [transform] function being invoked on each [Penta] element of original collection.
+ * Returns a single list of all elements yielded from results of [transform] function being invoked on each [Penta] element of the original collection.
+ *
+ * @param A the type of the first value in the penta
+ * @param B the type of the second value in the penta
+ * @param C the type of the third value in the penta
+ * @param D the type of the fourth value in the penta
+ * @param E the type of the fifth value in the penta
+ * @param R the type of the result elements
+ * @param transform the function to transform each penta into an iterable of results
+ * @return a list of all elements yielded from the transform function
  */
 inline fun <A, B, C, D, E, R> Iterable<Penta<A?, B?, C?, D?, E?>>.flatMap(
     transform: (A?, B?, C?, D?, E?) -> Iterable<R>
@@ -42,7 +72,17 @@ inline fun <A, B, C, D, E, R> Iterable<Penta<A?, B?, C?, D?, E?>>.flatMap(
 }
 
 /**
- * Returns a single list of all elements yielded from results of [transform] function being invoked on each [Hexa] element of original collection.
+ * Returns a single list of all elements yielded from results of [transform] function being invoked on each [Hexa] element of the original collection.
+ *
+ * @param A the type of the first value in the hexa
+ * @param B the type of the second value in the hexa
+ * @param C the type of the third value in the hexa
+ * @param D the type of the fourth value in the hexa
+ * @param E the type of the fifth value in the hexa
+ * @param F the type of the sixth value in the hexa
+ * @param R the type of the result elements
+ * @param transform the function to transform each hexa into an iterable of results
+ * @return a list of all elements yielded from the transform function
  */
 inline fun <A, B, C, D, E, F, R> Iterable<Hexa<A?, B?, C?, D?, E?, F?>>.flatMap(
     transform: (A?, B?, C?, D?, E?, F?) -> Iterable<R>
@@ -51,7 +91,18 @@ inline fun <A, B, C, D, E, F, R> Iterable<Hexa<A?, B?, C?, D?, E?, F?>>.flatMap(
 }
 
 /**
- * Returns a single list of all elements yielded from results of [transform] function being invoked on each [Hepta] element of original collection.
+ * Returns a single list of all elements yielded from results of [transform] function being invoked on each [Hepta] element of the original collection.
+ *
+ * @param A the type of the first value in the hepta
+ * @param B the type of the second value in the hepta
+ * @param C the type of the third value in the hepta
+ * @param D the type of the fourth value in the hepta
+ * @param E the type of the fifth value in the hepta
+ * @param F the type of the sixth value in the hepta
+ * @param G the type of the seventh value in the hepta
+ * @param R the type of the result elements
+ * @param transform the function to transform each hepta into an iterable of results
+ * @return a list of all elements yielded from the transform function
  */
 inline fun <A, B, C, D, E, F, G, R> Iterable<Hepta<A?, B?, C?, D?, E?, F?, G?>>.flatMap(
     transform: (A?, B?, C?, D?, E?, F?, G?) -> Iterable<R>
