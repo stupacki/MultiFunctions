a2e22bc5f5c71f380ecfceeb88de2ac295f360a2
Benny Schneider <stupacki86@gmail.com>
2025-03-14 20:37:34 +0100

    Optimize QuadSpec

--- multi-functions/src/test/kotlin/io/multifunctions/models/QuadSpec.kt
+++ multi-functions/src/test/kotlin/io/multifunctions/models/QuadSpec.kt
@@ -10,55 +10,42 @@ internal class QuadSpec : WordSpec() {
         "Quad" should {
 
             "be initialized with Strings" {
-
                 val testData = Quad("one", "two", "three", "four")
-
-                testData.let { quad ->
-                    quad.first shouldBe "one"
-                    quad.second shouldBe "two"
-                    quad.third shouldBe "three"
-                    quad.fourth shouldBe "four"
-
-                    Quad(quad.first, quad.second, quad.third, quad.fourth)
-                } shouldBe testData
+                verifyQuad(testData, "one", "two", "three", "four")
             }
 
             "handle null values" {
-
                 val testData = Quad<String?, String?, String?, String?>(null, null, null, null)
-
-                testData.let { quad ->
-                    quad.first shouldBe null
-                    quad.second shouldBe null
-                    quad.third shouldBe null
-                    quad.fourth shouldBe null
-
-                    Quad(quad.first, quad.second, quad.third, quad.fourth)
-                } shouldBe testData
+                verifyQuad(testData, null, null, null, null)
             }
 
             "handle toString" {
-
                 val actual = Quad("one", "two", "three", "four")
                 val expected = "Quad(first=one, second=two, third=three, fourth=four)"
-
                 actual.toString() shouldBe expected
             }
 
             "handle copy()" {
-
                 val actual = Quad("one", "two", "three", "four")
-
                 actual.copy() shouldBe actual
             }
 
             "handle toList" {
-
                 val actual = Quad("one", "two", "three", "four")
                 val expected = listOf("one", "two", "three", "four")
-
                 actual.toList() shouldBe expected
             }
         }
+    }
+
+    private fun <T> verifyQuad(
+        quad: Quad<T, T, T, T>,
+        first: T?, second: T?, third: T?, fourth: T?
+    ) {
+        quad.first shouldBe first
+        quad.second shouldBe second
+        quad.third shouldBe third
+        quad.fourth shouldBe fourth
+        Quad(quad.first, quad.second, quad.third, quad.fourth) shouldBe quad
     }
 }
