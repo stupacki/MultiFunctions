1b622500539b4518e53ce335aa25e0bd957112a7
Benny Schneider <stupacki86@gmail.com>
2025-03-14 20:20:34 +0100

    Optimize HexaSpec

--- multi-functions/src/test/kotlin/io/multifunctions/models/HexaSpec.kt
+++ multi-functions/src/test/kotlin/io/multifunctions/models/HexaSpec.kt
@@ -10,68 +10,46 @@ internal class HexaSpec : WordSpec() {
         "Hexa" should {
 
             "be initialized" {
-
                 val testData = Hexa("one", "two", "three", "four", "five", "six")
-
-                testData.let { hexa ->
-                    hexa.first shouldBe "one"
-                    hexa.second shouldBe "two"
-                    hexa.third shouldBe "three"
-                    hexa.fourth shouldBe "four"
-                    hexa.fifth shouldBe "five"
-                    hexa.sixth shouldBe "six"
-
-                    Hexa(hexa.first, hexa.second, hexa.third, hexa.fourth, hexa.fifth, hexa.sixth)
-                } shouldBe testData
+                verifyHexa(testData, "one", "two", "three", "four", "five", "six")
             }
 
             "handle null values" {
-
                 val testData = Hexa<String?, String?, String?, String?, String?, String?>(
-                    null,
-                    null,
-                    null,
-                    null,
-                    null,
-                    null
+                    null, null, null, null, null, null
                 )
-
-                testData.let { hexa ->
-                    hexa.first shouldBe null
-                    hexa.second shouldBe null
-                    hexa.third shouldBe null
-                    hexa.fourth shouldBe null
-                    hexa.fifth shouldBe null
-                    hexa.sixth shouldBe null
-
-                    Hexa(hexa.first, hexa.second, hexa.third, hexa.fourth, hexa.fifth, hexa.sixth)
-                } shouldBe testData
+                verifyHexa(testData, null, null, null, null, null, null)
             }
 
             "handle toString" {
-
                 val actual = Hexa("one", "two", "three", "four", "five", "six")
-                val expected =
-                    "Hexa(first=one, second=two, third=three, fourth=four, fifth=five, sixth=six)"
-
+                val expected = "Hexa(first=one, second=two, third=three, fourth=four, fifth=five, sixth=six)"
                 actual.toString() shouldBe expected
             }
 
             "handle copy()" {
-
                 val actual = Hexa("one", "two", "three", "four", "five", "six")
-
                 actual.copy() shouldBe actual
-
             }
 
             "handle toList" {
-
                 val actual = Hexa("one", "two", "three", "four", "fife", "six")
                 val expected = listOf("one", "two", "three", "four", "fife", "six")
-
                 actual.toList() shouldBe expected
             }
         }
+    }
+
+    private fun <T> verifyHexa(
+        hexa: Hexa<T, T, T, T, T, T>,
+        first: T?, second: T?, third: T?, fourth: T?, fifth: T?, sixth: T?
+    ) {
+        hexa.first shouldBe first
+        hexa.second shouldBe second
+        hexa.third shouldBe third
+        hexa.fourth shouldBe fourth
+        hexa.fifth shouldBe fifth
+        hexa.sixth shouldBe sixth
+        Hexa(hexa.first, hexa.second, hexa.third, hexa.fourth, hexa.fifth, hexa.sixth) shouldBe hexa
     }
 }
