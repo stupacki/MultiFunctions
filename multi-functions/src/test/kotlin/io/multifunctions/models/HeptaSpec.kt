e1e9af541ff33e76422d2c861ad95717038c80c3
Benny Schneider <stupacki86@gmail.com>
2025-03-14 20:19:59 +0100

    Optimize HeptaSpec

--- multi-functions/src/test/kotlin/io/multifunctions/models/HeptaSpec.kt
+++ multi-functions/src/test/kotlin/io/multifunctions/models/HeptaSpec.kt
@@ -11,82 +11,46 @@ internal class HeptaSpec : WordSpec() {
 
             "be initialized" {
                 val testData = Hepta("one", "two", "three", "four", "five", "six", "seven")
-
-                testData.let { hepta ->
-                    hepta.first shouldBe "one"
-                    hepta.second shouldBe "two"
-                    hepta.third shouldBe "three"
-                    hepta.fourth shouldBe "four"
-                    hepta.fifth shouldBe "five"
-                    hepta.sixth shouldBe "six"
-                    hepta.seventh shouldBe "seven"
-
-                    Hepta(
-                        hepta.first,
-                        hepta.second,
-                        hepta.third,
-                        hepta.fourth,
-                        hepta.fifth,
-                        hepta.sixth,
-                        hepta.seventh
-                    )
-                } shouldBe testData
+                verifyHepta(testData, "one", "two", "three", "four", "five", "six", "seven")
             }
 
             "handle null values" {
                 val testData = Hepta<String?, String?, String?, String?, String?, String?, String?>(
-                    null,
-                    null,
-                    null,
-                    null,
-                    null,
-                    null,
-                    null
+                    null, null, null, null, null, null, null
                 )
-
-                testData.let { hepta ->
-                    hepta.first shouldBe null
-                    hepta.second shouldBe null
-                    hepta.third shouldBe null
-                    hepta.fourth shouldBe null
-                    hepta.fifth shouldBe null
-                    hepta.sixth shouldBe null
-
-                    Hepta(
-                        hepta.first,
-                        hepta.second,
-                        hepta.third,
-                        hepta.fourth,
-                        hepta.fifth,
-                        hepta.sixth,
-                        hepta.seventh
-                    )
-                } shouldBe testData
+                verifyHepta(testData, null, null, null, null, null, null, null)
             }
 
             "handle toString" {
-
                 val actual = Hepta("one", "two", "three", "four", "five", "six", "seven")
-                val expected =
-                    "Hepta(first=one, second=two, third=three, fourth=four, fifth=five, sixth=six, seventh=seven)"
-
+                val expected = "Hepta(first=one, second=two, third=three, fourth=four, fifth=five, sixth=six, seventh=seven)"
                 actual.toString() shouldBe expected
             }
 
             "handle copy()" {
-
                 val actual = Hepta("one", "two", "three", "four", "five", "six", "seven")
-
                 actual.copy() shouldBe actual
             }
 
             "handle toList" {
-
                 val actual = Hepta("one", "two", "three", "four", "fife", "six", "seven")
                 val expected = listOf("one", "two", "three", "four", "fife", "six", "seven")
-
                 actual.toList() shouldBe expected
             }
         }
+    }
+
+    private fun <T> verifyHepta(
+        hepta: Hepta<T, T, T, T, T, T, T>,
+        first: T?, second: T?, third: T?, fourth: T?, fifth: T?, sixth: T?, seventh: T?
+    ) {
+        hepta.first shouldBe first
+        hepta.second shouldBe second
+        hepta.third shouldBe third
+        hepta.fourth shouldBe fourth
+        hepta.fifth shouldBe fifth
+        hepta.sixth shouldBe sixth
+        hepta.seventh shouldBe seventh
+        Hepta(hepta.first, hepta.second, hepta.third, hepta.fourth, hepta.fifth, hepta.sixth, hepta.seventh) shouldBe hepta
     }
 }
